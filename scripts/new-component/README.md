7a3dd969d72015794aa61fc5b087180d21e6022c
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-05 09:55:33 -0700

    Add new-component script

---
+++ scripts/new-component/README.md
@@ -0,0 +1,1 @@
+This code is adapted from Josh W Comeau's [new-component module](https://github.com/joshwcomeau/new-component).
---
+++ scripts/new-component/src/templates/ts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./COMPONENT_NAME.module.css";
+
+export type COMPONENT_NAMEProps = {};
+
+function COMPONENT_NAME({}: COMPONENT_NAMEProps) {
+  return <div></div>;
+}
+
+export default COMPONENT_NAME;
