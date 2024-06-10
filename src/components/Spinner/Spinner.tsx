e3a7a69f04e2a034387478822b09f59a5a1e30d4
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-06-09 18:30:30 -0700

    Add icon and styling for Spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,11 +1,16 @@
+import { Loader } from "lucide-react";
 import React from "react";
 
+import VisuallyHidden from "../VisuallyHidden";
 import styles from "./Spinner.module.css";
 
-export interface SpinnerProps {}
-
-function Spinner({}: SpinnerProps) {
-  return <div className={styles.wrapper}></div>;
-}
+const Spinner = () => {
+  return (
+    <div className={styles.wrapper}>
+      <VisuallyHidden>Loading...</VisuallyHidden>
+      <Loader size={48} className={styles.loader} />
+    </div>
+  );
+};
 
 export default Spinner;
