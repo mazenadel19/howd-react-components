8bb4559f335d3b6c47cec0dd4808caed74fadd5c
Mazen <mazenadel19@gmail.com>
2024-06-22 03:19:04 +0300

    2: Props and TypeScript

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
 
-export interface ButtonProps {}
+export interface ButtonProps extends React.ComponentProps<"button"> {
+  variant?: "filled" | "outline";
+}
 
-function Button({}: ButtonProps) {
-  return <div className={styles.wrapper}></div>;
+function Button({ variant = "filled", children }: Readonly<ButtonProps>) {
+  return <button className={styles.wrapper}>{children}</button>;
 }
 
 export default Button;
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Card.module.css';
+import styles from "./Card.module.css";
 
-export interface CardProps {}
+export interface CardProps extends React.ComponentProps<"article"> {
+  textColor: string;
+  backgroundColor: string;
+}
 
-function Card({}: CardProps) {
-  return <div className={styles.wrapper}></div>;
+function Card({ textColor, backgroundColor, children }: Readonly<CardProps>) {
+  return <article className={styles.wrapper}>{children}</article>;
 }
 
 export default Card;
